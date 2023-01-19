f1848ad6f609194910836f52cc8d7f839681255c
Ragnar Lodbrok <ragnar@MacBook-Air-Ragnar.local>
2023-01-19 14:05:32 +0500

    1 version

---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+def group_posts(request, slug):
+    group = get_object_or_404 (Group, slug=slug)
+
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
+
+
